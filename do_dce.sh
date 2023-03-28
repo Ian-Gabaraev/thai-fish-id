820e4e2190900456c3ce9f6bfc789e87a7378bed
Ian Gabaraev <39344857+Ian-Gabaraev@users.noreply.github.com>
2023-03-28 12:48:10 +0700

    Create do_dce.sh

---
+++ do_dce.sh
@@ -0,0 +1,6 @@
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+apt-cache policy docker-ce
+sudo apt install docker-ce
+sudo systemctl status docker
